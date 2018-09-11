085d785df4efc8ee4e549b6126a1485062e30fee
lipeng <lipeng@alllivemoble.net>
2018-09-11 12:24:05 +0800

    viewpage,可设置是佛偶可滑动

---
+++ app/src/main/java/com/xp/develop/customview/CustomViewPager.java
@@ -0,0 +1,44 @@
+package com.xp.develop.customview;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * author :  xpxn
+ * blog  :  https://blog.csdn.net/qq_38729449
+ * time  :  2018/9/3
+ * desc  :  utils about initialization
+ */
+public class CustomViewPager extends ViewPager{
+
+    private boolean isCanScroll = false;
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 设置其是否能滑动换页
+     * @param isCanScroll false 不能换页， true 可以滑动换页
+     */
+    public void setScanScroll(boolean isCanScroll) {
+        this.isCanScroll = isCanScroll;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return isCanScroll && super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return isCanScroll && super.onTouchEvent(ev);
+
+    }
+}
